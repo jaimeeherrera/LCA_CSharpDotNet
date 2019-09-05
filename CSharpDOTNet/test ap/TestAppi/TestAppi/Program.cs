b7c433d0894c7b67e69249c5cb3b478b0db11bd6
jaimeeherrera <jaimeeherrera22@gmail.com>
2019-09-04 19:13:05 -0500

    trying to push to git hub

---
+++ CSharpDOTNet/test ap/TestAppi/TestAppi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestAppi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test to push");
+        }
+    }
+}
